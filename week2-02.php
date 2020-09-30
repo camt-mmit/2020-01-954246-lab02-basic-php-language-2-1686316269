7f3a5692ad6804ae704546745730484392e30bba
612110264 <68628549+612110264ZheYin@users.noreply.github.com>
2020-09-30 11:09:23 +0700

    Add files via upload

---
+++ week2-02.php
@@ -0,0 +1,19 @@
+<?php
+  printf("input number: ");
+  fscanf(STDIN,"%d",$d);
+  // this must be $b <= 12
+  //for($b=1;$b<=11;$b++){
+    for($b=1;$b<=12;$b++){
+    for($a=2;$a<=$d;$a++) {
+      printf("%5d",$c=$a*$b);}
+  printf("\n");}
+
+
+  
+
+
+   
+
+ 
+  
+  
